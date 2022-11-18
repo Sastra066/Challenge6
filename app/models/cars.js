08f23b80acf19a21b0852ab5196e2233c250e983
unknown <sastraharapangulo2002@gmail.com>
2022-11-18 21:13:25 +0700

    createAt

--- app/models/cars.js
+++ app/models/cars.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     rentperday: DataTypes.INTEGER,
     type: DataTypes.STRING,
     image: DataTypes.STRING,
+    createAt: false
   }, {
     sequelize,
     modelName: 'cars',
